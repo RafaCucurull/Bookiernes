36a810ab55fa5626dfcf1ed7c9a8be086603e51b
Marc Resa Enrich <marcresaenrich@MacBook-MacBook-Pro-de-Marc.local>
2021-04-02 20:08:01 +0200

    Primers tres models

--- bookapp/admin.py
+++ bookapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from bookapp.models import Llibre, Escriptor, Editor
+
+
+class LlibreAdmin(admin.ModelAdmin):
+    pass
+
+
+class EscriptorAdmin(admin.ModelAdmin):
+    pass
+
+
+class EditorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Llibre, LlibreAdmin)
+admin.site.register(Escriptor, EscriptorAdmin)
+admin.site.register(Editor, EditorAdmin)
--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Llibre(models.Model):
+    nom_llibre = models.CharField(max_length=70)
+    escriptor = models.ForeignKey('Escriptor', on_delete=models.CASCADE)
+    editor = models.ForeignKey('Editor', on_delete=models.CASCADE)
+    portada = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom_llibre
+
+
+class Escriptor(models.Model):
+    nom_escriptor = models.CharField(max_length=20)
+    primer_cognom_escriptor = models.CharField(max_length=20)
+    segon_cognom_escriptor = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nom_escriptor + " " + self.primer_cognom_escriptor + " " + self.segon_cognom_escriptor
+
+
+class Editor(models.Model):
+    nom_editor = models.CharField(max_length=20)
+    primer_cognom_editor = models.CharField(max_length=20)
+    segon_cognom_editor = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nom_editor + " " + self.primer_cognom_editor + " " + self.segon_cognom_editor
