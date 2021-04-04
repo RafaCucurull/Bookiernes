d5056f1e6607a37bfff7df51e8649f00a1c54df5
Marc Resa Enrich <marcresaenrich@MacBook-MacBook-Pro-de-Marc.local>
2021-04-04 12:56:07 +0200

    Eliminats models editor i escriptor, afegida referencia a usuaris ja autenticats, afegida entitat consulta

--- bookapp/admin.py
+++ bookapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from bookapp.models import Llibre, Escriptor, Editor, TematiquesLlibre, Tematica
+from bookapp.models import Llibre, TematiquesLlibre, Tematica, Consulta
 
 
 class LlibreAdmin(admin.ModelAdmin):
@@ -23,8 +23,12 @@ class TematiquesLlibreAdmin(admin.ModelAdmin):
     pass
 
 
+class ConsultaAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Llibre, LlibreAdmin)
-admin.site.register(Escriptor, EscriptorAdmin)
-admin.site.register(Editor, EditorAdmin)
 admin.site.register(Tematica, TematicaAdmin)
 admin.site.register(TematiquesLlibre, TematiquesLlibreAdmin)
+admin.site.register(Consulta, ConsultaAdmin)
+
--- bookapp/models.py
+++ bookapp/models.py
@@ -3,32 +3,14 @@ from django.db import models
 
 class Llibre(models.Model):
     nom_llibre = models.CharField(max_length=70)
-    escriptor = models.ForeignKey('Escriptor', on_delete=models.CASCADE)
-    editor = models.ForeignKey('Editor', on_delete=models.CASCADE)
+    escriptor = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    editor = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
     portada = models.CharField(max_length=100)
     sinopsis = models.TextField(max_length=300)
     tematiques = models.ManyToManyField('Tematica', related_name='tematiques', blank=True, through='TematiquesLlibre')
 
     def __str__(self):
         return self.nom_llibre
-
-
-class Escriptor(models.Model):
-    nom_escriptor = models.CharField(max_length=20)
-    primer_cognom_escriptor = models.CharField(max_length=20)
-    segon_cognom_escriptor = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nom_escriptor + " " + self.primer_cognom_escriptor + " " + self.segon_cognom_escriptor
-
-
-class Editor(models.Model):
-    nom_editor = models.CharField(max_length=20)
-    primer_cognom_editor = models.CharField(max_length=20)
-    segon_cognom_editor = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nom_editor + " " + self.primer_cognom_editor + " " + self.segon_cognom_editor
 
 
 class Tematica(models.Model):
@@ -42,3 +24,9 @@ class Tematica(models.Model):
 class TematiquesLlibre(models.Model):
     llibre = models.ForeignKey('Llibre', on_delete=models.CASCADE)
     tematica = models.ForeignKey('Tematica', on_delete=models.CASCADE)
+
+
+class Consulta(models.Model):
+    usuari = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    assumpte = models.CharField(max_length=100)
+    cos = models.TextField()
