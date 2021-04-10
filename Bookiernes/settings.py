be094870667972df38efc3d000200c63f52797d9
rafa <rafacucugaz9911@gmail.com>
2021-04-10 14:10:07 +0200

    changes to resolve merge

--- Bookiernes/settings.py
+++ Bookiernes/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookapp',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,9 +126,3 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-AUTH_USER_MODEL = 'users.CustomUser'
-
-LOGIN_REDIRECT_URL = '/'
-
-LOGOUT_REDIRECT_URL = '/'
