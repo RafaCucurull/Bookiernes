f798f3d085a8763ae1ce7ff072713d102d3064bf
Marc Resa Enrich <marcresaenrich@MacBook-MacBook-Pro-de-Marc.local>
2021-04-11 18:07:28 +0200

    honeypot fet

--- Bookiernes/urls.py
+++ Bookiernes/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('secret/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('bookapp.urls')),
 ]
