fe75a87de91194260264f59f818a68212d2bfaa6
root <root@LAPTOP-K8D8G0S9.localdomain>
2021-04-15 00:53:09 +0200

    Arreglos

--- bookapp/forms.py
+++ bookapp/forms.py
@@ -6,7 +6,12 @@ from bookapp.models import Llibre
 class AfegirLlibreForm(ModelForm):
     class Meta:
         model = Llibre
-        fields = ('pdf', 'nom_llibre', 'tematiques', 'sinopsis')
+        fields = ('pdf', 'nom_llibre', 'tematiques', 'sinopsis','coleccio','num_pagines')
         widgets = {'nom_llibre': Textarea(attrs={'cols': 45, 'rows': 1}),
-                   'sinopsis': Textarea(attrs={'cols': 50, 'rows': 10}),
-                   'tematiques': CheckboxSelectMultiple}
+                   'sinopsis': Textarea(attrs={'cols': 50, 'rows': 6}),
+                   'tematiques': CheckboxSelectMultiple,
+                   'coleccio':  Textarea(attrs={'cols': 45, 'rows': 1}),
+                   'num_pagines': Textarea(attrs={'cols': 45, 'rows': 1}),
+                   }
+        
+
