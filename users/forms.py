b11431e58b992b9794a8572a25c5e66777fc9676
Robert Torres Tosquella <robert_torres@protonmail.com>
2021-05-28 16:01:16 +0200

    Backend registre lectors

--- users/forms.py
+++ users/forms.py
@@ -17,6 +17,20 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('email', 'is_Editor', 'is_Escriptor', 'is_Maquetacio', 'is_IT', 'password1', 'password2')
 
+class CustomLectorCreationForm(UserCreationForm):
+    name = forms.CharField(help_text="Introdueix el seu nom i cognoms (p.ex. Carlos Ruíz Zafon)")
+    email = forms.CharField(help_text="Introdueix un format correcte de correu electrònic (p.ex. cruizz@escriptors.cat)")
+    password1 = forms.CharField(
+        help_text=["La teva contrassenya no pot ser similar a l'altra informació personal teva.",
+                   "La teva contrassenya ha de tenir almenys 8 caràcters.",
+                   "La teva contrassenya no pot ser una comunment usada.",
+                   "La teva contrassenya no pot ser completament numèrica."])
+    password2 = forms.CharField(help_text="Introdueix la mateixa contrassenya que anteriorment, per verificació.")
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('nme', 'email', 'password1', 'password2')
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import login
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from users.forms import CustomUserCreationForm
+from users.forms import CustomUserCreationForm, CustomLectorCreationForm
 
 
 def register(request):
@@ -16,3 +16,16 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+def registerLector(request):
+    if request.method == 'POST':
+        form = CustomLectorCreationForm(request.POST)
+        print (form.is_valid())
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomLectorCreationForm()
+    return render(request, 'registration/registerLector.html', {'form': form})
