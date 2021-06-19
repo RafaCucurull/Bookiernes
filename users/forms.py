64c78cae70d4ab2a76d0e79f014bf73cfe62cb26
rafa <rafacucugaz9911@gmail.com>
2021-06-19 22:22:10 +0200

    configuracio template

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import TextInput
 
 from .models import CustomUser
 from django import forms
@@ -41,4 +42,21 @@ class ConfiguracioForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('nom', 'edat', 'sexe')
+        widgets = {
+            'nom': TextInput(attrs={
+                'class': "form-control",
+                'style': 'width: 100%;background-color: linen;font-size: 30px; margin: auto',
+                'placeholder': 'El seu nom...'
+            }),
+            'edat': TextInput(attrs={
+                'class': "form-control",
+                'style': 'width: 35%;background-color: linen;font-size: 30px; margin: auto',
+                'placeholder': 'La seva edat...'
+            }),
+            'sexe': TextInput(attrs={
+                'class': "form-control",
+                'style': 'width: 50%;background-color: linen;font-size: 30px; margin: auto',
+                'placeholder': 'El seu sexe...'
+            }),
+        }
 
